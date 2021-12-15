f67f9bcd23d6d93ca0774eaa130b5fde1050e199
Kevin Flanagan <flanagankp@gmail.com>
2021-12-15 13:21:00 -0600

    basic vue app

---
+++ frontend/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Dashboard from '../views/Dashboard.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
